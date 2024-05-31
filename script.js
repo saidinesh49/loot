9f6c2e014cfbd91304da146d4c83580bba4f4cf2
saidinesh49 <dineshpalisetty49@gmail.com>
2024-05-31 13:01:05 +0530

    Added entry

---
+++ script.js
@@ -0,0 +1,10 @@
+function addsignin(){
+    document.querySelector('.login').style.display='';
+    document.querySelector('.signup').style.display='none';
+
+    }
+    let globalusername;
+function addsignup(){
+    document.querySelector('.signup').style.display='';
+    document.querySelector('.login').style.display='none';
+}
